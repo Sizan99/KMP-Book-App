58ada0a8f57d70efbf01f0da50458ab4b1f13852
Shiam Ahmed Sizan <sizan.szn@gmail.com>
2025-02-20 15:02:09 +0600

    Publish

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
@@ -4,31 +4,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import com.plcoding.bookpedia.book.data.network.KtorRemoteBookDataSource
-import com.plcoding.bookpedia.book.data.repository.DefaultBookRepository
 import com.plcoding.bookpedia.book.presentation.book_list.BookListScreenRoot
 import com.plcoding.bookpedia.book.presentation.book_list.BookListViewModel
-import com.plcoding.bookpedia.core.data.HttpClientFactory
-import io.ktor.client.engine.HttpClientEngine
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.viewmodel.koinViewModel
 
 
 @Composable
 @Preview
-fun App(engine: HttpClientEngine) {
+fun App() {
+    val viewModel = koinViewModel<BookListViewModel>()
     Scaffold(
         modifier = Modifier.fillMaxSize()
     ) { innerpadding ->
         BookListScreenRoot(
-            viewModel = remember {
-                BookListViewModel(
-                    bookRepository = DefaultBookRepository(
-                        remoteBookDataSource = KtorRemoteBookDataSource(
-                            httpClient = HttpClientFactory.create(engine)
-                        )
-                    )
-                )
-            },
+            viewModel = viewModel,
             onBookClick = {}
         )
 
